bd982c48bc8979282f8c1368b1216d7f5759aca5
Rileyrcs <r.c.sess@icloud.com>
2020-06-10 18:22:00 -0400

    Initial Commit

---
+++ CustomLoginDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Carter Sessions on 6/10/20.
+//  Copyright © 2020 Carter Sessions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
